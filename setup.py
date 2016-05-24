da30d81f96536244f6444b5836c78f467eb57b4b
pws <patrick.wilmes@bit-lake.com>
2016-05-24 15:47:10 +0200

    - setup py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='commonspy',
+    version='1.0-DEV',
+    packages=['test', 'commons'],
+    url='',
+    license='GNU GPL v3',
+    author='Patrick Wilmes',
+    author_email='patrick.wilmes@bit-lake.com',
+    description='Commons for python 3'
+)
