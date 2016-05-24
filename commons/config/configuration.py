c71b4ad44baae8eb2941c752797e5b0bf5b4a335
pws <patrick.wilmes@bit-lake.com>
2016-05-24 16:11:41 +0200

    - json config draft

--- commons/config/configuration.py
+++ commons/config/configuration.py
@@ -1,4 +1,5 @@
 import configparser
+import json
 import os
 
 
@@ -15,3 +16,21 @@ class IniConfig:
         if key not in self.parser[section]:
             raise Exception('Key {} is not in section {}!'.format(key, section))
         return self.parser[section][key]
+
+
+class JsonConfig:
+    def __init__(self, configfile):
+        if not os.path.isfile(configfile):
+            raise Exception('The file {} does not exist!'.format(configfile))
+        with open(configfile) as file:
+            self.json = json.load(file)
+
+    def get_property(self, key):
+        keys = key.split('.')
+        json_tmp = self.json
+        for inner_key in keys:
+            if inner_key in json_tmp:
+                json_tmp = json_tmp[inner_key]
+            else:
+                raise Exception('Key {} does not exist!'.format(inner_key))
+        return json_tmp
