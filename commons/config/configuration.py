0e199ed52eb26a57a4760412dca513c83f51b11a
pws <patrick.wilmes@bit-lake.com>
2016-05-24 16:02:32 +0200

    - get property impl

--- commons/config/configuration.py
+++ commons/config/configuration.py
@@ -9,5 +9,9 @@ class ConfigUtils:
         self.parser = configparser.ConfigParser()
         self.parser.read(configfile)
 
-    def get_property(self, key):
-        pass
+    def get_property(self, section, key):
+        if section not in self.parser:
+            raise Exception('Section {} does not exist in config file!'.format(section))
+        if key not in self.parser[section]:
+            raise Exception('Key {} is not in section {}!'.format(key, section))
+        return self.parser[section][key]
