87f0ad423266da1ffa2a820aeb0d46cd3a81bd69
pws <patrick.wilmes@bit-lake.com>
2016-05-24 16:07:38 +0200

    - rename again

--- test/config/configuration_test.py
+++ test/config/configuration_test.py
@@ -1,25 +1,25 @@
 import unittest
 
-from commons.config.configuration import Config
+from commons.config.configuration import IniConfig
 
 
 class ConfigUtilsTest(unittest.TestCase):
     def test_get_property_no_such_section(self):
-        config = Config('test.ini')
+        config = IniConfig('test.ini')
         with self.assertRaises(Exception):
             config.get_property('Some', 'none')
 
     def test_get_property_key_does_not_exist(self):
-        config = Config('test.ini')
+        config = IniConfig('test.ini')
         with self.assertRaises(Exception):
             config.get_property('TestSection', 'none')
 
     def test_config_file_does_not_exist(self):
         with self.assertRaises(Exception):
-            Config('doesnotexist.ini')
+            IniConfig('doesnotexist.ini')
 
     def test_get_property_key_section_exists(self):
-        config = Config('test.ini')
+        config = IniConfig('test.ini')
         result = config.get_property('TestSection', 'testproperty')
 
         self.assertEquals(result, 'test')
