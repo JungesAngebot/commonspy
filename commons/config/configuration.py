c392bb435e9026b1e38ebc562678de92e3ca8103
pws <patrick.wilmes@bit-lake.com>
2016-05-24 16:23:21 +0200

    - pydoc

--- commons/config/configuration.py
+++ commons/config/configuration.py
@@ -4,13 +4,33 @@ import os
 
 
 class IniConfig:
+    """
+    This utils class helps you to get properties out of an ini file.
+    Validation if part of this config class.
+    """
     def __init__(self, configfile):
+        """
+        Create an instance of the config class with a config file. The constructor will
+        validate if the specified file exists. If not it will raise an exception.
+        Otherwise the configuration file (ini) will be parsed for later access.
+
+        :param configfile: ini file to parse
+        """
         if not os.path.isfile(configfile):
             raise Exception('The file {} does not exist!'.format(configfile))
         self.parser = configparser.ConfigParser()
         self.parser.read(configfile)
 
     def get_property(self, section, key):
+        """
+        Checks if the specified section exists in the configuration file and checks also if the specified
+        key exists in the configuration. If one of the two conditions is not met the method will
+        raise an exception. Otherwise the value of the property will be returned.
+
+        :param section: config section in the ini file
+        :param key: key in the specified config section
+        :return: value of the config key
+        """
         if section not in self.parser:
             raise Exception('Section {} does not exist in config file!'.format(section))
         if key not in self.parser[section]:
@@ -19,13 +39,39 @@ class IniConfig:
 
 
 class JsonConfig:
+    """
+    This class handles json based configuration. It parses a given json file for
+    accessing different values in it.
+    """
     def __init__(self, configfile):
+        """
+        The constructor of the json config will validate if the specified file exists. If not it
+        will raise an exception. Otherwise the configuration will be read and parsed for
+        later processing / value retrieval.
+
+        :param configfile: json file to load
+        """
         if not os.path.isfile(configfile):
             raise Exception('The file {} does not exist!'.format(configfile))
         with open(configfile) as file:
             self.json = json.load(file)
 
     def get_property(self, key):
+        """
+        This method provides you access to the configuration file. You can access a value by specifying the
+        correct json key. You can also chain the keys:
+        {
+          "simple": "value",
+          "chain": {
+            "some": {
+              "key": "value"
+            }
+          }
+        }
+        If you want to access the inner "key": "value" property just provide a key like: chain.some.key.
+        :param key: key or key chain to access values
+        :return: value of the specified key or key chain
+        """
         keys = key.split('.')
         json_tmp = self.json
         for inner_key in keys:
