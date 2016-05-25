7b950fdb4646743b9480b2cc7678c8df944ba05b
pws <patrick.wilmes@bit-lake.com>
2016-05-25 14:54:20 +0200

    - inline

--- commonspy/http.py
+++ commonspy/http.py
@@ -1,5 +1,4 @@
 import urllib.request
 
 
-def download_file(url, desitination):
-    urllib.request.urlretrieve(url, desitination)
+def download_file(url, desitination): urllib.request.urlretrieve(url, desitination)
