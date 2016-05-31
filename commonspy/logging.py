5976e47734dcce8a6a080f924d3be38e5045d79a
pws <patrick.wilmes@bit-lake.com>
2016-05-31 17:15:26 +0200

    - better logging

--- commonspy/logging.py
+++ commonspy/logging.py
@@ -1,7 +1,20 @@
 import datetime
 import json
+import logging
 import socket
 from threading import current_thread
+
+from pythonjsonlogger import jsonlogger
+
+logger = logging.getLogger()
+logging.basicConfig(level=logging.DEBUG)
+
+
+log_handler = logging.StreamHandler(open('api.log.json', 'w', encoding='utf-8'))
+formatter = jsonlogger.JsonFormatter()
+log_handler.setFormatter(formatter)
+log_handler.setLevel(logging.DEBUG)
+logger.addHandler(log_handler)
 
 
 class Message:
@@ -13,12 +26,15 @@ class Message:
         '40': 'Error'
     }
 
-    def __init__(self, message, level):
+    def __init__(self, message):
         self.timestamp = str(datetime.datetime.now())
         self.message = message
-        self.level = level
+        self.level = self._logging_level()
         self.hostname = socket.getfqdn()
         self.thread_name = current_thread().getName()
+
+    def _logging_level(self):
+        return Message.level[str(logger.level)]
 
     def __str__(self):
         return json.dumps(self.__dict__)
