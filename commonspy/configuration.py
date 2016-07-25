9a330ff3826799a86fd67354a3b5699be0978cb1
pws <patrick.wilmes@bit-lake.com>
2016-07-25 13:24:09 +0200

    -  moved decorator

--- commonspy/configuration.py
+++ commonspy/configuration.py
@@ -89,16 +89,4 @@ class JsonConfig:
         return json_tmp
 
 
-def deprecated(func):
-    """This is a decorator which can be used to mark functions
-    as deprecated. It will result in a warning being emmitted
-    when the function is used."""
 
-    @functools.wraps(func)
-    def new_func(*args, **kwargs):
-        warnings.simplefilter('always', DeprecationWarning)
-        warnings.warn("Call to deprecated function {}.".format(func.__name__), category=DeprecationWarning, stacklevel=2)
-        warnings.simplefilter('default', DeprecationWarning)
-        return func(*args, **kwargs)
-
-    return new_func
--- commonspy/decorators.py
+++ commonspy/decorators.py
@@ -1,6 +1,22 @@
 import functools
+import warnings
 
 from flask import jsonify
+
+
+def deprecated(func):
+    """This is a decorator which can be used to mark functions
+    as deprecated. It will result in a warning being emmitted
+    when the function is used."""
+
+    @functools.wraps(func)
+    def new_func(*args, **kwargs):
+        warnings.simplefilter('always', DeprecationWarning)
+        warnings.warn("Call to deprecated function {}.".format(func.__name__), category=DeprecationWarning, stacklevel=2)
+        warnings.simplefilter('default', DeprecationWarning)
+        return func(*args, **kwargs)
+
+    return new_func
 
 
 def json(function):
