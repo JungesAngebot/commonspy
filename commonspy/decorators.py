f61c93034a4f0ca67b0909ade055429c99529594
pythonixpp <patrick.wilmes@bit-lake.com>
2016-08-15 15:02:17 +0200

    - format

--- commonspy/decorators.py
+++ commonspy/decorators.py
@@ -8,7 +8,7 @@ from flask import jsonify
 def traced_call(func):
     @functools.wraps(func)
     def wrapped(*args, **kwargs):
-        log_debug('Call %s ... with args %s and kwargs %s.' % (func.__name__, args, kwargs))
+        log_debug('Call %s with args %s and kwargs %s.' % (func.__name__, args, kwargs))
         return func(*args, **kwargs)
     return wrapped
 
