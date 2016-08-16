43cc33cd86d493b9166e82deba6a7a934cb4cbc7
pythonixpp <patrick.wilmes@bit-lake.com>
2016-08-16 14:06:13 +0200

    - basic url building

--- commonspy/request.py
+++ commonspy/request.py
@@ -1,7 +1,9 @@
-
 """ Module for communication between clients and the mongo db middleware. """
 
 
 def exec_http_get(base_url, endpoint, content_id=None, page=0, size=10):
-    pass
+    endpoint_url = '%s/%s' % (base_url, endpoint)
+    if content_id is not None:
+        endpoint_url = '%s/%s' % (endpoint_url, content_id)
+
 
