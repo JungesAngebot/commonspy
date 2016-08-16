5489c82c5028d33d851144c1ce99377c4b6d360e
pythonixpp <patrick.wilmes@bit-lake.com>
2016-08-16 14:46:30 +0200

    - auth in test script

--- extra/request_tests.py
+++ extra/request_tests.py
@@ -1,7 +1,9 @@
 from pprint import pprint
 
+from requests.auth import HTTPBasicAuth
+
 from commonspy.request import exec_http_get
 
-json = exec_http_get('http://localhost:8080/read', 'stories')
+json = exec_http_get('http://localhost:8080/read', 'stories', auth=HTTPBasicAuth('juan', 'juanHackt'))
 
 pprint(json)
