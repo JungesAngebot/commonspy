e8daa91df4317a6a8ce6a9b1ba4587f3c36bf337
pythonixpp <patrick.wilmes@bit-lake.com>
2016-08-16 14:46:10 +0200

    - more flex auth

--- commonspy/request.py
+++ commonspy/request.py
@@ -1,15 +1,14 @@
 """ Module for communication between clients and the mongo db middleware. """
 import requests
-from requests.auth import HTTPBasicAuth
 
 error_http_codes = (400, 404, 500)
 
 
-def exec_http_get(base_url, endpoint, content_id=None, page=0, size=10, auth=HTTPBasicAuth('juan', 'juanHackt')):
+def exec_http_get(base_url, endpoint, content_id=None, page=0, size=10, auth=None):
     """ Executes a http get request to the mongo middleware.
     """
     request_url = _build_request_url(base_url, content_id, endpoint, page, size)
-    response = requests.get(request_url, auth=auth)
+    response = requests.get(request_url, auth=auth) if auth is not None else requests.get(request_url)
     if response.status_code in error_http_codes:
         pass
     return response.json()['_embedded'][endpoint] if content_id is None else response.json()
