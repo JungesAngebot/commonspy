e969cecf789b81c07f791420cb1003ac4254f811
pythonixpp <patrick.wilmes@bit-lake.com>
2016-08-16 14:08:28 +0200

    - sending get request

--- commonspy/request.py
+++ commonspy/request.py
@@ -1,8 +1,10 @@
 """ Module for communication between clients and the mongo db middleware. """
+import requests
 
 
 def exec_http_get(base_url, endpoint, content_id=None, page=0, size=10):
     request_url = _build_request_url(base_url, content_id, endpoint, page, size)
+    response = requests.get(request_url)
 
 
 def _build_request_url(base_url, content_id, endpoint, page, size):
