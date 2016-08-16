7418bef0d1fe99aa86d43b2150654d5c7f573129
pythonixpp <patrick.wilmes@bit-lake.com>
2016-08-16 14:13:16 +0200

    - basic doc

--- commonspy/request.py
+++ commonspy/request.py
@@ -6,6 +6,9 @@ error_http_codes = (400, 404, 500)
 
 
 def exec_http_get(base_url, endpoint, content_id=None, page=0, size=10):
+    """ Executes a http get request to the mongo middleware.
+
+    """
     request_url = _build_request_url(base_url, content_id, endpoint, page, size)
     response = requests.get(request_url)
     if response.status_code in error_http_codes:
