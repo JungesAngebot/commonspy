2bc50dee5a7051296953678d08024277b3d1c8c3
patrickwilmes <patrick.wilmes@bit-lake.com>
2016-11-03 14:10:02 +0100

    - release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='commonspy',
-    version='1.2.0.0-DEV',
+    version='1.2.0.0-RELEASE',
     packages=['commonspy'],
     url='',
     license='GNU GPL v3',
