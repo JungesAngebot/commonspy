d4ab38ddb1a442db33b794a53bbc657e06f4e8d4
patrickwilmes <patrick.wilmes@bit-lake.com>
2016-11-03 14:11:45 +0100

    - new dev version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='commonspy',
-    version='1.2.0.0-RELEASE',
+    version='1.3.0.0-DEV',
     packages=['commonspy'],
     url='',
     license='GNU GPL v3',
